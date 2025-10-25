90865e0dc17a642e9a930773a16ba80b80bc7135
Hugo Alexander <165641301+Galexhu@users.noreply.github.com>
2025-10-25 15:48:34 +0000

    final fix

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -10,58 +10,68 @@ const Home = () => {
 	const aumentarUnidad = () => {
 
 		//setUnidad(numero+1) 
-		setUnidad((previusNumber)=> previusNumber + 1 )
+		setUnidad((previusNumber) => previusNumber + 1)
 	}
-	useEffect(()=>{
-		if (unidad === 10 ){
-			setDecena((previusDecena)=> previusDecena + 1)
+	useEffect(() => {
+		if (unidad === 10) {
+			setDecena((previusDecena) => previusDecena + 1)
 			setUnidad(0)
 		}
-		
-	},[unidad])
 
-	useEffect(()=>{
-		if (decena === 10){
-			setCentena((previusCentena)=> previusCentena + 1)
+	}, [unidad])
+
+	useEffect(() => {
+		if (decena === 10) {
+			setCentena((previusCentena) => previusCentena + 1)
 			setDecena(0)
 		}
 
-	},[decena])
+	}, [decena])
 
-	useEffect(()=>{
-		if (centena === 10){
-			setMillar((previusMillar)=> previusMillar + 1)
+	useEffect(() => {
+		if (centena === 10) {
+			setMillar((previusMillar) => previusMillar + 1)
 			setCentena(0)
 		}
 
-	},[centena])
+	}, [centena])
 
-	useEffect(()=>{
-		let intervalo = setInterval(aumentarUnidad,500)
-		return ()=> clearInterval(intervalo)
-	
-	},[])
+	useEffect(() => {
+		let intervalo = setInterval(aumentarUnidad, 500)
+		return () => clearInterval(intervalo)
+
+	}, [])
 
 	const reiniciarContador = () => {
 		setUnidad(0)
 		setDecena(0)
 		setCentena(0)
 		setMillar(0)
-	
+
 	}
 
 
 	return (
 		<div className="text-center">
-		<div className="d-flex justify-content-center">
-			<p> {millar} </p>	
-			<p> {centena} </p>
-			<p> {decena} </p>			
-        	<p> {unidad} </p>
+			<div className="d-flex justify-content-center p-3 mb-2 bg-black text-white">
+				<i className="card-number-1 display-1 fa-solid fa-clock"></i>
+				<div className="card-number display-1">
+					<p>{millar}</p>
+				</div>
+				<div className="card-number display-1">
+					<p>{centena}</p>
+				</div>
+				<div className="card-number display-1">
+					<p>{decena}</p>
+				</div>
+				<div className="card-number display-1">
+					<p>{unidad}</p>
+				</div>
+
+			</div>
+			<button className="btn btn-primary" onClick={reiniciarContador}>RESET</button>
 		</div>
-		<button className="btn btn-primary" onClick={reiniciarContador}>reiniciar contador</button>	
-		</div>
-		
+
 	);
 };
 
